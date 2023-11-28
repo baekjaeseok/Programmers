f7792cc43b4d9b6c90efa7c367704b2a5c8afa74
백재석 <tomatoraub@naver.com>
2023-11-28 19:55:03 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        int multi = 1;
-        int plus = 0;
+        int product = 1;
+        int sum = 0;
         for(int i = 0; i < num_list.length; i++) {
-            multi *= num_list[i];
-            plus += num_list[i];
+            product *= num_list[i];
+            sum += num_list[i];
         }
         
-        if(multi < plus * plus) {
+        if(product < sum * sum) {
             answer = 1;
         } else {
             answer = 0;
