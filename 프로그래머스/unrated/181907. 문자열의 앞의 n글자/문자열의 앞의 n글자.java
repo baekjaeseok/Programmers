3e8ba57f0eb39dfe646baa30a99305acafec1617
백재석 <tomatoraub@naver.com>
2023-12-02 19:21:06 +0900

    [level unrated] Title: 문자열의 앞의 n글자, Time: 0.04 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -1,7 +1,14 @@
 class Solution {
     public String solution(String my_string, int n) {
-        String answer = "";
-        answer += my_string.substring(0, n);
-        return answer;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < n; i++) {
+            char c = my_string.charAt(i);
+            sb.append(c);
+        }
+        
+        return sb.toString();
+
     }
 }
