9a273980e2a1b6f3924960ef88fa9b15c274e9e0
백재석 <tomatoraub@naver.com>
2023-12-02 16:00:01 +0900

    Update 등차수열의 특정한 항만 더하기.java
    Refactoring

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -9,3 +9,16 @@ class Solution {
         return answer;
     }
 }
+
+// Refactoring
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        for (int i = 0; i < included.length; i++) {
+            if(included[i]) {
+                answer += a + d * i;
+            }
+        }
+        return answer;
+    }
+}
