464dea0246d58de96c643925fd8f416228e9ee3a
백재석 <tomatoraub@naver.com>
2023-12-02 19:22:12 +0900

    Update n 번째 원소까지.java
    refactoring

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -8,3 +8,17 @@ class Solution {
         return answer;
     }
 }
+
+// Refactoring
+class Solution {
+    public String solution(String my_string, int n) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < n; i++) {
+            char c = my_string.charAt(i);
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
