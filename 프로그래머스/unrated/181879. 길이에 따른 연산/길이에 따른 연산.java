09221cdd9a27a4e5fefb925291d0a21d5fe1f00b
백재석 <tomatoraub@naver.com>
2023-12-02 20:04:29 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -2,20 +2,17 @@ class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
         int len = num_list.length;
-        int sum = 0;
-        int multiply = 1;
         
         // 배열의 길이가 11 이상이면 모든 원소의 합을 구하시오
         if (len >= 11) {
             for (int i = 0; i < len; i++) {
-                sum += num_list[i];
-                answer = sum;
+                answer += num_list[i];
             }
         // 배열의 길이가 11 보다 작담녀 모든 원소를 곱하시오    
         } else {
+            answer = 1;
             for (int i = 0; i < len; i++) {
-                multiply *= num_list[i];
-                answer = multiply;
+                answer *= num_list[i];
             }
         }
         return answer;        
