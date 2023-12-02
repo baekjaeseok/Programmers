e2dac37d57fb51341b740b30e109062af1e50233
백재석 <tomatoraub@naver.com>
2023-12-02 20:07:45 +0900

    [level unrated] Title: 소문자로 바꾸기, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(myString.toLowerCase());             
+        
+        return sb.toString();
+
+    }
+}
