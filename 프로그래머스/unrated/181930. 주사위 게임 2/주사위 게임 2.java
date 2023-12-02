76171bd9a3ad27af9bd99d0a39e04eccac97c403
백재석 <tomatoraub@naver.com>
2023-12-02 16:31:52 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -2,11 +2,11 @@ class Solution {
     public int solution(int a, int b, int c) {
         int answer = 0;        
         
-        if (a != b && a != c && b != c) {
+		if (a != b && a != c && b != c) {
 			answer = a + b + c;
-		} else if((a == b && a !=c || b != c) || (b == c && b != a || c != a) ||                     (c == a && c != b || a !=b)) {
+		} else if((a == b && b != c) || (b == c && c != a) || (a == c && a !=b)) {
 			answer = (a+b+c) * (a*a + b*b + c*c);
-		} else if(a == b && a == c && b == c) {
+		} else if(a == b && a == c) {
 			answer = (a+b+c) * (a*a + b*b + c*c) * (a*a*a+b*b*b+c*c*c);
 		}
 		
