91f57cfd552c12d3cb198849294e4172f532f126
백재석 <tomatoraub@naver.com>
2023-12-02 20:05:24 +0900

    Update 길이에 따른 연산.java
    refactoring

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -8,7 +8,29 @@ class Solution {
             for (int i = 0; i < len; i++) {
                 answer += num_list[i];
             }
-        // 배열의 길이가 11 보다 작담녀 모든 원소를 곱하시오    
+        // 배열의 길이가 11 보다 작다면 모든 원소를 곱하시오    
+        } else {
+            answer = 1;
+            for (int i = 0; i < len; i++) {
+                answer *= num_list[i];
+            }
+        }
+        return answer;        
+    }
+}
+
+// Refactoring
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int len = num_list.length;
+        
+        // 배열의 길이가 11 이상이면 모든 원소의 합을 구하시오
+        if (len >= 11) {
+            for (int i = 0; i < len; i++) {
+                answer += num_list[i];
+            }
+        // 배열의 길이가 11 보다 작다면 모든 원소를 곱하시오    
         } else {
             answer = 1;
             for (int i = 0; i < len; i++) {
